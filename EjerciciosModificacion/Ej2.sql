b4de5e3ae8b847dca4de12c4c9a27663a341f698
Jesica606 <jesicaaranibar17@gmail.com>
2023-09-07 10:44:31 -0300

    Ejercicio 2 de Modificaciones

---
+++ EjerciciosModificacion/Ej2.sql
@@ -0,0 +1,6 @@
+--2) Inserts de usuario y post
+insert into user (id, username, password)
+values ('1', 'Anabelle', 'pass');
+
+insert into post ( author_id, title, body)
+values (1, 'Historias', 'Habia una vez, hace mucho tiempo, etc');
