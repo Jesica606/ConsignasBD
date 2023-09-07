f1bd3961cab8e3afbb9919d8979179eca2c54a11
Jesica606 <jesicaaranibar17@gmail.com>
2023-09-07 11:11:21 -0300

    Agrege mas usuarios y post

--- EjerciciosModificacion/Ej2.sql
+++ EjerciciosModificacion/Ej2.sql
@@ -1,6 +1,7 @@
 --2) Inserts de usuario y post
+--2) Inserts de usuario y post
 insert into user (id, username, password)
-values ('1', 'Anabelle', 'pass');
-
-insert into post ( author_id, title, body)
-values (1, 'Historias', 'Habia una vez, hace mucho tiempo, etc');
+values (1, 'Anabelle', 'pass'), (2, 'Mariana', 'pass2'), (3, 'Diego', 'pass3'), (4, 'Diana', 'pass4');
+ 
+insert into post (author_id, title, body)
+values (1, 'Historias', 'Habia una vez, hace mucho tiempo, etc'), (2, 'Tarea', 'Ejercicio 1, Ejercicio 2, Ejercicio 3, Ejercicio 4, Ejercicio 5'), (2, 'Matematicas', 'Ejercicio 1+2+3+4+5+6+7'), (3, 'Clima', 'Lluvia a las 13, y sol a las 16'), (4, 'Diario', 'Hoy perdi mi lapiz');
