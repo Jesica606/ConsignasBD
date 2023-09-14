cc775b556aa609cabdc37067d27f2378d8a0dd73
Jesica606 <jesicaaranibar17@gmail.com>
2023-09-14 10:38:49 -0300

    Ejercicio 1 de Insert

---
+++ EjerciciosInsert/EjercicioIns1.sql
@@ -0,0 +1,4 @@
+--Insertar tres ciudades de argentina a elección en un solo insert 
+insert into user (id, username, password)
+values (1, 'Anabelle', 'pass'), (2, 'Mariana', 'pass2'), (3, 'Diego', 'pass3'), (4, 'Diana', 'pass4');
+ 
